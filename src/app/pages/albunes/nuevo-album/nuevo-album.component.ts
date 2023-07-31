426338f21ac8f097fc9aa873ed222a030cb3dee3
scorcueramel <scorcueramel@gmail.com>
2023-07-30 19:48:22 -0500

    diseño de carga de imagenes

--- src/app/pages/albunes/nuevo-album/nuevo-album.component.ts
+++ src/app/pages/albunes/nuevo-album/nuevo-album.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
-import { DomSanitizer } from '@angular/platform-browser';
 import { Album } from 'src/app/interfaces/album';
 import { AlbumService } from 'src/app/services/album.service';
 import { SwalService } from 'src/app/services/swal.service';
@@ -13,94 +12,108 @@ import { SwalService } from 'src/app/services/swal.service';
   providers: [MessageService],
 })
 export class NuevoAlbumComponent implements OnInit {
-  public archivos: any = [];
-  public previsualiza!: string;
-  public fileTmp: any;
-  public body = new FormData();
-
-  album: Album = {
-    nombre: '',
-  };
-
   constructor(
     private router: Router,
-    private messageService: MessageService,
-    private sanitizer: DomSanitizer,
     private albumService: AlbumService,
-    private swalService: SwalService
-  ) {}
+    private swalService: SwalService,
+    private messageService: MessageService,
+    ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {  }
 
-  onSelectFile(event: any) {
-    const archivoCapturado = event.target.files[0];
+  archivos = new Array();
+  imagen!: string;
+  previsualizar = new Array();
+  album: Album = {
+    nombre: '',
+    portada: '',
+    publicado: true,
+  };
 
-    this.extraerBase64(archivoCapturado).then((imagen: any) => {
-      this.previsualiza = imagen.base;
-    });
+  processFiles(event: any): void {
+    let headers = new Headers();
+    // con esto enviar archivos y texto ENCTYPE
+    headers.append('enctype', 'multipart/form-data;');
+    headers.append('Authorization', `Bearer ${localStorage.getItem('token')}`);
 
-    this.archivos.push(archivoCapturado);
+    this.swalService.wait();
 
-    this.fileTmp = {
-      fileRaw: this.archivos[0],
-      fileName: this.archivos[0].name,
+    this.archivos.push(event.target.files);
+
+    if (this.archivos.length > 0) {
+      const formData: FormData = new FormData();
+      formData.append(`portada`, this.archivos[0][0], this.archivos[0][0].name);
+
+      this.previsualizacion(event);
+
+      this.albumService.storeImage({ headers: headers }, formData).subscribe({
+        next: (resp: any) => {
+          this.album.portada = resp.archivo.filename;
+          console.log(resp);
+          this.swalService.close();
+        },
+        error: (err: any) => {
+          console.log(err.error);
+          this.swalService.close();
+        },
+      });
+    }
+  }
+
+  guardar() {
+    let headers = new Headers();
+    headers.append('content-type', 'application/json');
+    headers.append('Authorization', `Bearer ${localStorage.getItem('token')}`);
+    this.swalService.wait();
+    this.albumService
+      .registerAlbum({ headers: headers }, this.album)
+      .subscribe({
+        next: (resp: any) => {
+          console.log(resp);
+          this.show(
+            'Albúm Creado',
+            'success',
+            'Se creo el nuevo album  ',
+            ''
+            );
+          this.swalService.close()
+        },
+        error: (err: any) => {
+          this.swalService.close()
+          this.show(
+            'No Creado',
+            'error',
+            'No se pudo crear el albúm contacte con soporte  ',
+            ''
+            );
+          console.log(err.error);
+        },
+      });
+  }
+
+  previsualizacion(file: any): void {
+    let archivos = file.target.files;
+    let reader = new FileReader();
+    reader.readAsDataURL(archivos[0]);
+    reader.onload = () => {
+      this.previsualizar.pop();
+      this.previsualizar.push(reader.result);
     };
   }
 
-  // onSelectFile(event: any): any {
-
-  //   const archivoCapturado = event.target.files[0];
-
-  //   this.extraerBase64(archivoCapturado).then((imagen: any) => {
-  //     this.previsualiza = imagen.base;
-  //   });
-
-  //   this.archivos.push(archivoCapturado);
-
-  // }
-
-  // onRegister() {
-  //   let headers = new Headers();
-
-  //   headers.append('Content-Type', 'application/json');
-  //   headers.append('Authorization', `Bearer ${localStorage.getItem('token')}`);
-
-  //   this.body.append('nombre', this.album.nombre);
-  //   this.body.append('portada', this.fileTmp.fileRaw,this.fileTmp.fileName);
-
-  //   this.albumService.registerAlbum({ headers: headers }, this.body).subscribe({
-  //     next: (resp: any) => {
-  //       console.log(resp);
-  //     },
-  //     error: (err): any => {
-  //       console.log(err);
-  //     },
-  //   });
-  // }
-
-  extraerBase64 = async ($event: any) =>
-    new Promise((resolve): void => {
-      try {
-        const unsafeImg = window.URL.createObjectURL($event);
-        const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
-        const reader = new FileReader();
-        reader.readAsDataURL($event);
-        reader.onload = () => {
-          resolve({
-            base: reader.result,
-          });
-        };
-        reader.onerror = (error) => {
-          resolve({
-            base: null,
-          });
-        };
-      } catch (error) {
-        error;
-      }
-    });
+  eliminarFoto() {
+    this.previsualizar = [];
+  }
 
   volver(): void {
     this.router.navigate(['/menu/inicio/albunes/todos']);
+  }
+
+  show(summary: any, severity: any, mensaje: any, name: any):void {
+    this.messageService.add({
+      severity: `${severity}`,
+      summary: `${summary}`,
+      detail: `${mensaje} ${name}`,
+    });
   }
 }
